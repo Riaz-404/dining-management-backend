d875048a8ddebab3523b1ea381b4ae4bc1d7c948
Riaz-404 <riazulislam.cuc@gmail.com>
2024-11-13 19:20:02 +0600

    middleware setup

--- src/app.js
+++ src/app.js
@@ -1,6 +1,14 @@
-import express from "express";
+import express, {urlencoded} from "express";
+import cors from "cors";
 
 const app = express();
+
+app.use(express.json({limit: "16kb"}));
+app.use(urlencoded({extended: true, limit: "16kb"}));
+app.use(cors({
+  url: process.env.CORS_ORIGIN,
+  credentials: true,
+}));
 
 app.use("/", (req, res) => {
   res.send("Hello");
